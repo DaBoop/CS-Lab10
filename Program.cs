0e0019584fc6d5046e7acafb90caa73593ec4d93
DaBoop <arti_2002_2011@mail.ru>
2020-11-12 02:08:32 +0300

    Task1 Demo

--- Program.cs
+++ Program.cs
@@ -6,15 +6,22 @@ namespace Lab10
     {
         static void Main(string[] args)
         {
+            Image air, fire;
             Image[] imgArr =
                 {
                     new Image(200,200,"png", "water"),
                     new Image(100,200, "png", "earth"),
-                    new Image(300,200, "jpg", "fire"),
-                    new Image(150,200, "png", "air")
+                    fire = new Image(300,200, "jpg", "fire"),
+                    air = new Image(150,200, "png", "air")
                 };
             var imgList = new ImgList(imgArr);
             Console.WriteLine(imgList);
+
+            imgList.Add(new Image());
+            imgList.Remove(fire);
+            Console.WriteLine(imgList);
+            Console.WriteLine(imgList.Find(air));
+
         }
     }
 }
